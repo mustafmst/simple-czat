baaf692b4647abe8df70a668263cc1c054e77fce
Pawel Mstowski <pawel.mstowski@gmail.com>
2018-05-05 20:44:25 +0200

    Setup handlebars rendering

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname+'/views/layouts',
+    partialsDir: __dirname+'/views/partials'
+}));
+
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('index', {
+        helpers: {
+            foo: function() {
+                console.log("FOO!");
+            }
+        }
+    });
 });
 
 app.listen(3000, () => {
---
+++ public/front.js
@@ -0,0 +1,3 @@
+function onClickFoo() {
+    alert("FOO!!!");
+}
